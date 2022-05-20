8d700403b9c402a07d8e0209ec3bd0ae2b117e2e
ryuken73 <ryuken73@gmail.com>
2022-05-20 12:54:15 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const fs = require('fs');
+const path = require('path');
+const restify = require('restify');
+const morgan = require('morgan');
+const errors = require('restify-errors')
+const corsMiddleware = require('restify-cors-middleware');
+
+const outPath = './upload';
+
+const loggerConfig = {};
+const {notification={}, logLevel="DEBUG", logFile="logger.log"} = loggerConfig;
+const loggerOptions = {
+  notification,
+  logLevel,
+  logFile
+}
+
+global.logger = require('./lib/logger')(loggerOptions);
+logger = global.logger;
+
+const cors = corsMiddleware({
+  origins: ['*'],
+});
+
+const server = restify.createServer();
+server.pre(cors.preflight);
+server.use(cors.actual);
+server.use(morgan('combined'));
+server.use(restify.plugins.queryParser());
+// server.use(restify.plugins.bodyParser());
+// server.pre(restify.plugins.pre.context());
+
+server.post('/sendFile/:fname', (req, res) => {
+  logger.info(req.url);
+  const targetFile = path.join(outPath, req.params.fname);
+  const wStream = fs.createWriteStream(targetFile);
+  logger.info(`inFile = ${req.params.fname} outFile = ${targetFile}`);
+  req.pipe(wStream);
+  req.on('end', reason => logger.info(`request end..`, reason));
+  req.on('close', () => {
+    logger.info(`request closed[${req.url}], destoryed = ${req.destroyed}`);
+    if(req.destroyed){
+      wStream.close();
+    }
+  }) 
+  req.on('error', (error) => {
+    logger.error(`request error[${req.url}], error = ${error}`);
+    wStream.close();
+  })
+  wStream.on('finish', () => {
+    logger.info(`finish: ${req.params.fname}`);
+    res.send('done');
+  })
+  wStream.on('close', () => {
+    logger.info('stream closed');
+  });
+});
+
+server.on('InternalServer', function (req, res, err, cb) {
+  logger.error('internal error occurred!');
+  err.toString = function() {
+    return 'an internal server error occurred!';
+  };
+  err.toJSON = function() {
+    return {
+      message: 'an internal server error occurred!',
+      code: 'error'
+    }
+  };
+  return cb();
+});
+
+server.on('restifyError', function (req, res, err, cb) {
+  return cb();
+});
+
+server.listen(7000, () => {
+  logger.info(`listening ${server.name}: ${server.url}`);
+});
+
+process.on('warning', e => logger.warn(e.stack));
